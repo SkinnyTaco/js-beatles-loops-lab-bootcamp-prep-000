4af32f58e56075603a85ad91dd55d70ca5cfc7ef
Daniel Dawson <dannyldawson@gmail.com>
2018-11-10 02:27:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,24 @@
 function theBeatlesPlay(arrayMusicians, arrayInstruments) {
   let array = [];
   for (let i = 0; i < arrayMusicians.length; i++) {
-    array.push(``)
+    array.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`)
   }
+  return array;
+}
+
+function johnLennonFacts(arrayFacts) {
+  let i = 0;
+  while (i < arrayFacts.length) {
+    let fact = arrayFacts[i] + '!!!';
+    arrayFacts[i] = fact;
+  }
+  return arrayFacts;
+}
+
+function ILoveTheBeatles(num) {
+  let array = [];
+  do {
+    array.push('I love the Beatles!');
+  } while (num < 15);
+  return array;
 }
